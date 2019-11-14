72935309db7e3a34f8ba92727a073d3b025ffa52
Amani Berkani <amaniberkani@icloud.com>
2019-11-14 17:59:18 +0100

    quete finie

--- TestDate.java
+++ TestDate.java
@@ -3,6 +3,7 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Date;
 import java.util.Scanner; 
+import java.text.ParseException;
 
 class TestDate {
 
@@ -18,6 +19,23 @@ class TestDate {
         sc.close();
 
         // TODO : your code after this line
-        
+
+	String stringDate = String.valueOf(month) + "-" + String.valueOf(day) + "-" + String.valueOf(year);
+	SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+	Date date = new Date();
+	
+	try {
+		
+		date = sdf.parse(stringDate);
+	}
+	catch (ParseException e){
+
+		e.printStackTrace();
+	}
+
+	Calendar calendar =new GregorianCalendar(year, month - 1, day);
+	int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+	System.out.println("The date is : "+ sdf.format(date));
+	System.out.println("The day of week is : "+ dayOfWeek);
     }
 }
